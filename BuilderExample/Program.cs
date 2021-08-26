b8fdb096341f6f6c3956e59c0349827d78c7b3a6
betir <betirov95@gmail.com>
2021-08-26 20:47:44 +0300

    Добавьте файлы проекта.

---
+++ BuilderExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuilderExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
